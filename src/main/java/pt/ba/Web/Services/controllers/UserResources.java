c32634a10a941358cf78b336330aeb5d4bcee50d
BernardoArede <bernardoarede04@gmail.com>
2025-06-23 19:15:57 +0100

    add: service layer

---
+++ src/main/java/pt/ba/Web/Services/controllers/UserResources.java
@@ -0,0 +1,33 @@
+package pt.ba.Web.Services.controllers;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pt.ba.Web.Services.entities.User;
+import pt.ba.Web.Services.services.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserResources {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+   @GetMapping("/{id}")
+   public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj =  service.findById(id);
+       return ResponseEntity.ok().body(obj); 
+   }
+
+}
--- src/main/java/pt/ba/Web/Services/repositories/UserRepository.java
+++ src/main/java/pt/ba/Web/Services/repositories/UserRepository.java
@@ -1,6 +1,11 @@
 package pt.ba.Web.Services.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import pt.ba.Web.Services.entities.User;
 
-public interface UserRepository extends JpaRepository <User,Long> {}
+@Repository //Podemos tirar mas vamos deixar para esclarecer
+public interface UserRepository extends JpaRepository <User,Long> {
+
+}
