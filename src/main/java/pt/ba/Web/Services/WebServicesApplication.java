88b1da60379bc381658b56b1b3d2511f9458bf7a
BernardoArede <bernardoarede04@gmail.com>
2025-06-23 12:49:40 +0100

    First Commit

---
+++ src/main/java/pt/ba/Web/Services/WebServicesApplication.java
@@ -0,0 +1,13 @@
+package pt.ba.Web.Services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/ba/Web/Services/WebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ba.Web.Services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
